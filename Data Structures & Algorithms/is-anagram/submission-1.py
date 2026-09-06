541bfaac55d5d3a6563e15de7c1d47aae2b1066a
qasim10amar <100310148+qasim10amar@users.noreply.github.com>
2026-09-05 20:15:02 -0600

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        count = {}
+        if len(s) != len(t):
+            return False
+        for char in s:
+            if char in count:
+                count[char] +=1
+            else:
+                count[char] = 1
+        for char in t:
+            if char in count:
+                count[char] -= 1
+            else:
+                return False
+        for value in count.values():
+            if value != 0:
+                return False
+        return True
+        
+        
